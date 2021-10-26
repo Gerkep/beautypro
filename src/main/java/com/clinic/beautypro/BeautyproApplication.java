0366b6f74c07c7688b9f88b117a3eaa32585789b
Piotr Gerke <piotrg2003@gmail.com>
2021-10-26 17:05:38 +0200

    first commit

---
+++ src/main/java/com/clinic/beautypro/BeautyproApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.beautypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinic/beautypro/BeautyproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.beautypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
