7e1e75341c2d56f1813bd0fab4fe1eecff60189d
Piotr Gerke <piotrg2003@gmail.com>
2021-11-03 21:25:18 +0100

    pricing page

---
+++ src/main/resources/static/js/pricingAnimation.js
@@ -0,0 +1,13 @@
+const startAnimation = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("active", entry.isIntersecting);
+    });
+  };
+  
+  const observer = new IntersectionObserver(startAnimation);
+  const options = { root: null, rootMargin: '40px', threshold: 1 }; 
+  
+  const elements = document.querySelectorAll('.treatments-container');
+  elements.forEach(el => {
+    observer.observe(el, options);
+  });
