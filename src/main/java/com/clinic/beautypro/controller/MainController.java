1fdbcbc1383ff04c07642d9089a9c3057e779166
Piotr Gerke <piotrg2003@gmail.com>
2021-11-12 20:56:22 +0100

    email sending

--- src/main/java/com/clinic/beautypro/controller/MainController.java
+++ src/main/java/com/clinic/beautypro/controller/MainController.java
@@ -1,11 +1,22 @@
 package com.clinic.beautypro.controller;
 
+import com.clinic.beautypro.mail.EmailService;
+import com.clinic.beautypro.model.MyMail;
+import com.sendgrid.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    EmailService emailService;
 
     @RequestMapping("/")
     public String home() {
@@ -16,8 +27,22 @@ public class MainController {
     public String pricingPage() {
         return "pricing.html";
     }
+
     @GetMapping("/transformations")
     public String transformationsPage() {
         return "transformations.html";
+    }
+
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        model.addAttribute("mail", new MyMail("", ""));
+        return "index";
+    }
+
+    @PostMapping("/send")
+    public String sendMail(@ModelAttribute MyMail mail, Model model) {
+        model.addAttribute("mail", mail);
+        Response response = emailService.sendEmail(mail);
+        return "confirmation";
     }
 }
