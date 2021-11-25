6ef858a77edcf67fc48c0bd163064eaedf5360ab
Piotr Gerke <piotrg2003@gmail.com>
2021-11-25 16:31:20 +0100

    desktop version

--- src/main/resources/static/js/Animation.js
+++ src/main/resources/static/js/Animation.js
@@ -1,13 +1,15 @@
-const startAnimation = (entries, observer) => {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("active", entry.isIntersecting);
+if(screen.width<1200){
+  const startAnimation = (entries, observer) => {
+      entries.forEach(entry => {
+        entry.target.classList.toggle("active", entry.isIntersecting);
+      });
+    };
+    
+    const observer = new IntersectionObserver(startAnimation);
+    const options = { root: null, rootMargin: '120px', threshold: 1 }; 
+    
+    const elements = document.querySelectorAll('.stage-container, .treatments-container, .testimonials-block, .container, .transformation, .opinion-container');
+    elements.forEach(el => {
+      observer.observe(el, options);
     });
-  };
-  
-  const observer = new IntersectionObserver(startAnimation);
-  const options = { root: null, rootMargin: '120px', threshold: 1 }; 
-  
-  const elements = document.querySelectorAll('.stage-container, .treatments-container, .testimonials-block, .container, .transformation, .opinion-container');
-  elements.forEach(el => {
-    observer.observe(el, options);
-  });
+}
